03560de6c2921a7530dbdaf9fbd3a2d46cc27406
Laura <laura.e.vollmer@gmail.com>
2024-03-27 10:54:50 -0500

    added my public index.html file back into my code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar';// Import the NavBar component
 
 function App() {
   return (
     <div >
+      <NavBar />
       <h1>Hello</h1>
     </div>
   );
