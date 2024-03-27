4d481f115f14d4a43e2f20a1bfec3c5e5d162c88
Laura <laura.e.vollmer@gmail.com>
2024-03-27 12:15:11 -0500

    styling navigation bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom"; //import Link (creating links) from react-router-dom for routing functionality
 import IconMenu from "../assets/images/icon-menu.svg"; //import the icon-menu.svg file from the assets/images folder
+import IconClose from "../assets/images/icon-menu-close.svg"; //import the icon-close.svg file from the assets/images folder
 import { useState } from "react"; //import the useState hook from React
 
 const NavBar = () => {
@@ -19,18 +20,21 @@ const NavBar = () => {
       {/* when isOpen is true then translatex-0 appears showing the sidebar */}
       {/* when the screen is a medium size or larger the sidebar is always visible and positioned normally */}
       <nav
-        className={`fixed top-0 right-0 h-full bg-white-800 transition-transform transform ${
+        className={`fixed top-0 right-0 w-3/4 bg-white flex transition-transform transform ${
           isOpen ? "translate-x-0" : "translate-x-full"
         } md:relative md:translate-x-0 md:w-auto`}
       >
+        {/* Close Icon to close the side navigation bar when in mobile */}
+        <button className="md:hidden top-0 right-0" onClick={() => setIsOpen(false)}>
+          <img src={IconClose} alt="close icon" />
+        </button>
+        <div className="flex-col">
         <Link to="/">Home</Link>
         <Link to="/new">New</Link>
         <Link to="/popular">Popular</Link>
         <Link to="/trending">Trending</Link>
         <Link to="/categories">Categories</Link>
-        <button className="md:hidden" onClick={() => setIsOpen(false)}>
-          Close
-        </button>
+        </div>
       </nav>
       <nav className="flex flex-row items-center justify-between p-5">
         <Link to="/">Home</Link>
