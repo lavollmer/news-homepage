70f054cadeb8068503e5c4725d4a11a601c7bc97
Laura <laura.e.vollmer@gmail.com>
2024-03-27 12:02:51 -0500

    working on navigation bar mobile responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom"; //import Link (creating links) from react-router-dom for routing functionality
 import IconMenu from "../assets/images/icon-menu.svg"; //import the icon-menu.svg file from the assets/images folder
+import { useState } from "react"; //import the useState hook from React
 
 const NavBar = () => {
   //useState hook from React - determines whether the sidebar is open or not
@@ -8,11 +9,17 @@ const NavBar = () => {
   return (
     <div>
       <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
-        Menu
-      </button>{" "}
-      {/* button to toggle the sidebar on mobile devices */}
-      <img src={IconMenu} alt="menu icon when application is mobile" />{" "}
-      {/* image of the menu icon */}
+        <img src={IconMenu} alt="menu icon when application is mobile" />
+        {/* image of the menu icon */}
+      </button>
+      <nav
+        className={`fixed top-0 right-0 h-full bg-white-800 transition-transform transform ${
+          isOpen ? "translate-x-0" : "translate-x-full"
+        } md:relative md:translate-x-0 md:w-auto`}
+      ></nav>
+      <button className="md:hidden" onClick={() => setIsOpen(false)}>
+        Close
+      </button>
       <nav className="flex flex-row items-center justify-between p-5">
         <Link to="/">Home</Link>
         <Link to="/new">New</Link>
