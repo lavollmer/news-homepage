827f538c08fc069e81d18c4a4c8f76d406a85192
Laura <laura.e.vollmer@gmail.com>
2024-03-27 14:03:12 -0500

    adding grid properties to components on app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css"; // Import the App.css file
 import Icon from "./components/Icon"; //import the Icon component
 import NavBar from "./components/NavBar"; // Import the NavBar component
-import SplashPage from "./components/SplashPage";// Import the SplashPage component
+import SplashPage from "./components/SplashPage"; // Import the SplashPage component
 import NewSection from "./components/NewSection"; // Import the NewSection component
 
 function App() {
   return (
     <div>
       <div className="flex flex-row">
-        <Icon />
-        <NavBar />
+          <Icon />
+          <NavBar />
+        </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <SplashPage />
+        <NewSection />
       </div>
-      <SplashPage />
-      <NewSection />
     </div>
   );
 }
