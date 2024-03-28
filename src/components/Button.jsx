6026b017384b8343a4dea00a0ce1c8895d87d484
Laura <laura.e.vollmer@gmail.com>
2024-03-28 09:24:28 -0500

    updating button size and making letter wider apart

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,8 @@ const Button = () => {
   return (
     <div>
       {/* Button HTML tag to create a button with the label of Read More */}
-      <button className='p-5 bg-orange-600 hover:bg-orange-700 font-bold py-2 px-4'>
-        <h1 className='text-white text-sm'>READ MORE</h1>
+      <button className='p-5 bg-orange-600 hover:bg-orange-700 font-bold py-2 px-8'>
+        <h1 className='text-white text-xs tracking-widest'>READ MORE</h1>
         </button>
     </div>
   )
