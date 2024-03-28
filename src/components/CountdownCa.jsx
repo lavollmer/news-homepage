410a572a5b72565318d79ec23b70cd2b960f6974
Laura <laura.e.vollmer@gmail.com>
2024-03-28 09:46:46 -0500

    completed the Countdown section

--- src/components/CountdownCa.jsx
+++ src/components/CountdownCa.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Retro from "../assets/images/image-retro-pcs.jpg";
+import Laptops from "../assets/images/image-top-laptops.jpg";
+import Gaming from "../assets/images/image-gaming-growth.jpg";
 
 const CountdownCa = () => {
   return (
@@ -8,17 +10,45 @@ const CountdownCa = () => {
         <div className="col-span-1 md:col-span-2">
           <img src={Retro} alt="Retro PCs" />
         </div>
-        <div className="col-span-3 md:col-span-4 pl-2">
-            <h1 className="text-gray-400 font-bold text-2xl">01</h1>
-            <h2 className="text-black-800 font-bold text-md ">
-                Reviving Retro PCs
-            </h2>
-            <p className="text-gray-800 text-sm ">
-                What happens when old PCs are given modern upgrades?
-            </p>
+        <div className="col-span-3 md:col-span-4 pl-2 space-y-1">
+          <h1 className="text-gray-400 font-bold text-2xl">01</h1>
+          <h2 className="text-black-800 font-bold text-md ">
+            Reviving Retro PCs
+          </h2>
+          <p className="text-gray-600 text-sm ">
+            What happens when old PCs are given modern upgrades?
+          </p>
         </div>
       </div>
+      <div className="grid grid-cols-4 md:grid-cols-6">
+        <div className="col-span-1 md:col-span-2">
+          <img src={Laptops} alt="Top of laptop" />
+        </div>
+        <div className="col-span-3 md:col-span-4 pl-2 space-y-1">
+          <h1 className="text-gray-400 font-bold text-2xl">01</h1>
+          <h2 className="text-black-800 font-bold text-md ">
+            Top 10 Laptops of 2022
+          </h2>
+          <p className="text-gray-600 text-sm ">
+            Our best picks for various needs and budgets.
+          </p>
+        </div>
       </div>
+      <div className="grid grid-cols-4 md:grid-cols-6">
+        <div className="col-span-1 md:col-span-2">
+          <img src={Gaming} alt="Gaming controller" />
+        </div>
+        <div className="col-span-3 md:col-span-4 pl-2 space-y-1">
+          <h1 className="text-gray-400 font-bold text-2xl">01</h1>
+          <h2 className="text-black-800 font-bold text-md ">
+            The Growth of Gaming
+          </h2>
+          <p className="text-gray-600 text-sm ">
+            How the pandemic has sparked fresh opportunities.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
