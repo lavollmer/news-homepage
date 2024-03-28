d9bbcc69993c234e83950e47f204119a9e34fbe1
Laura <laura.e.vollmer@gmail.com>
2024-03-28 12:01:21 -0500

    debugging navbar mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,39 +8,49 @@ const NavBar = () => {
   //useState hook from React - determines whether the sidebar is open or not
   const [isOpen, setIsOpen] = useState(false); //useState hook to manage the state of the navbar
   return (
-    <div className="flex flex-row pt-5 pb-10 justify-end">
+    // <div className="flex flex-row pt-5 pb-10 justify-end">
+    <div>
       {/* Menu icon img is only visible when the screen is smaller than medium - when toggled isOpen state opening or closing sidebar */}
-      <button
-        className="md:hidden block top-0 right-0"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <img
-          src={IconMenu}
-          alt="menu icon displays when application is mobile"
-        />
-      </button>
-      {/* navigation icon is fixed to top right of screen, width of 64 units and height of full */}
-      {/* when isOpen is true then translatex-0 appears showing the sidebar */}
-      {/* when the screen is a medium size or larger the sidebar is always visible and positioned normally */}
-      <nav
-        className={`fixed top-0 right-0 w-3/4 bg-[#FEFDF9] flex transition-transform transform ${
-          isOpen ? "translate-x-0" : "translate-x-full"
-        } md:relative md:translate-x-0 md:w-auto`}
-      >
-        {/* Close Icon to close the side navigation bar when in mobile */}
         <button
-          className="md:hidden top-0 right-0"
-          onClick={() => setIsOpen(false)}
+          className="md:hidden block top-0 right-0"
+          onClick={() => setIsOpen(!isOpen)}
         >
-          <img src={IconClose} alt="close icon" />
+          <img
+            src={IconMenu}
+            alt="menu icon displays when application is mobile"
+          />
         </button>
-        <div className="flex flex-row text-[#98959C] font-inter-regular justify-end space-x-10 w-full pb-10">
-          <Link to="/" className="hover:text-black">Home</Link>
-          <Link to="/new" className="hover:text-black">New</Link>
-          <Link to="/popular" className="hover:text-black">Popular</Link>
-          <Link to="/trending" className="hover:text-black">Trending</Link>
-          <Link to="/categories" className="hover:text-black">Categories</Link>
-        </div>
+        {/* navigation icon is fixed to top right of screen, width of 64 units and height of full */}
+        {/* when isOpen is true then translatex-0 appears showing the sidebar */}
+        {/* when the screen is a medium size or larger the sidebar is always visible and positioned normally */}
+
+        <nav
+          className={`fixed top-0 right-0 w-full h-full bg-white transform transition-transform duration-200 ease-in-out 
+          ${isOpen ? `translate-x-0` : `translate-x-ful`}`}>
+          {/* Close Icon to close the side navigation bar when in mobile */}
+          <button
+            // className="md:hidden top-0 right-0"
+            onClick={() => setIsOpen(false)}
+          >
+            <img src={IconClose} alt="close icon" />
+          </button>
+          <div className="flex flex-col text-[#98959C] font-inter-regular justify-end space-y-10 w-full pb-10">
+            <Link to="/" className="hover:text-black" onClick={() => setIsOpen(false)}>
+              Home
+            </Link>
+            <Link to="/new" className="hover:text-black" onClick={() => setIsOpen(false)}>
+              New
+            </Link>
+            <Link to="/popular" className="hover:text-black" onClick={() => setIsOpen(false)}>
+              Popular
+            </Link>
+            <Link to="/trending" className="hover:text-black" onClick={() => setIsOpen(false)}>
+              Trending
+            </Link>
+            <Link to="/categories" className="hover:text-black" onClick={() => setIsOpen(false)}>
+              Categories
+            </Link>
+          </div>
       </nav>
     </div>
   );
